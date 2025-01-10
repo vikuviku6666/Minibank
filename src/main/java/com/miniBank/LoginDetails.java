3444987b94b71977f8c4eed3ef7d655294961eff
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2025-01-10 13:08:31 +0100

    variable added to login details

--- src/main/java/com/miniBank/LoginDetails.java
+++ src/main/java/com/miniBank/LoginDetails.java
@@ -4,11 +4,13 @@ public class LoginDetails
 {
     private final String ssn;
     private final String password;
+    private final String name;
 
     public LoginDetails(String ssn, String password, String name)
     {
         this.ssn = ssn;
         this.password = password;
+        this.name = name;
     }
 
     public String getSsn()
